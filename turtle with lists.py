98f282c720f4b4b8f32974a75a101e81b3f4f2c9
Paladin-of-divine-windows <78314258+Paladin-of-divine-windows@users.noreply.github.com>
2021-02-13 12:03:38 +0200

    Додано функції які малюють цифри 3 і 4, також внесено зміни в функцію 1. раніше коли одиничка не стояла на першому місці, піднімався весь числовий ряд на 50 пунктків - виправлено дану помиклу

--- turtle with lists.py
+++ turtle with lists.py
@@ -6,19 +6,30 @@ numbers_for_post_index = [x + 1 for x in range(0, 10) ]
 turtle_rotation = [45, 90, 135, 180, 225, 270, 315, 360]
 turtle_movement = [20, 50, 71, 100]
 
-
-def number_one(movment, rotation):
-    '''The function draws one. '''
-    
-    turtle.left(turtle_rotation[0])
-    turtle.forward(turtle_movement[2])
-    turtle.right(turtle_rotation[2])
-    turtle.forward(turtle_movement[3])
-    turtle.right(turtle_rotation[3])
-    turtle.forward(turtle_movement[3])
+def next_number ():
     turtle.penup()
     turtle.right(turtle_rotation[1])
     turtle.forward(turtle_movement[0])
+
+
+def number_one():
+    '''The function draws one. '''
+   
+    turtle.penup()
+    turtle.forward(turtle_movement[1])
+    turtle.pendown()
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[3])
+    turtle.right(turtle_rotation[3])
+    turtle.forward(turtle_movement[3])
+    turtle.left(turtle_rotation[2])
+    turtle.forward(turtle_movement[2])
+    turtle.left(turtle_rotation[2])
+    turtle.penup()
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    next_number()
 
 
 def number_two():
@@ -33,17 +44,38 @@ def number_two():
     turtle.penup()
     turtle.left(turtle_rotation[1])
     turtle.forward(turtle_movement[3])
-    turtle.right(turtle_rotation[1])
-    turtle.forward(turtle_movement[0])
-
+    next_number()
 
 
 def number_three():
-    pass
+    turtle.pendown()
+    turtle.forward(turtle_movement[1])
+    turtle.right(turtle_rotation[2])
+    turtle.forward(turtle_movement[2])
+    turtle.left(turtle_rotation[2])
+    turtle.forward(turtle_movement[1])
+    turtle.right(turtle_rotation[2])
+    turtle.forward(turtle_movement[2])
+    turtle.penup()
+    turtle.left(turtle_rotation[2])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[3])
+    next_number()
 
 
 def number_four():
-    pass
+    turtle.pendown()
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[3])
+    turtle.forward(turtle_movement[3])
+    next_number()
+
 
 
 def number_five():
@@ -71,8 +103,9 @@ def number_zero():
 
 
 def post_index_maker():
-    number_one(turtle_movement, turtle_rotation)
-    number_two()
+    number_one()
+    number_three()
+    number_four()
 
 post_index_maker()
 turtle.penup()
