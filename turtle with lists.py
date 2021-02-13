56ad83e6a089860517699044598015f1c7f88ffd
Paladin-of-divine-windows <78314258+Paladin-of-divine-windows@users.noreply.github.com>
2021-02-13 13:38:37 +0200

    Створено всі функції які будуть мальвати цифри. додано функцію яка переміщує черепашку в попочаткову позицію, до того як почати писати цифри

--- turtle with lists.py
+++ turtle with lists.py
@@ -6,6 +6,11 @@ numbers_for_post_index = [x + 1 for x in range(0, 10) ]
 turtle_rotation = [45, 90, 135, 180, 225, 270, 315, 360]
 turtle_movement = [20, 50, 71, 100]
 
+
+def start_pozition():
+    turtle.penup()
+    turtle.backward(turtle_movement[3]*4)
+
 def next_number ():
     turtle.penup()
     turtle.right(turtle_rotation[1])
@@ -77,36 +82,117 @@ def number_four():
     next_number()
 
 
-
 def number_five():
-    pass
+    turtle.pendown()
+    turtle.forward(turtle_movement[1])
+    turtle.backward(turtle_movement[1])
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.backward(turtle_movement[1])
+    turtle.forward(turtle_movement[1])
+    turtle.penup()
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[3])
+    next_number()
 
 
 def number_six():
-    pass
+    turtle.penup()
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.pendown()
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.right(turtle_rotation[2])
+    turtle.forward(turtle_movement[2])
+    turtle.left(turtle_rotation[0])
+    next_number()  
 
 
 def number_seven():
-    pass
+    turtle.pendown()
+    turtle.forward(turtle_movement[1])
+    turtle.right(turtle_rotation[2])
+    turtle.forward(turtle_movement[2])
+    turtle.left(turtle_rotation[0])
+    turtle.forward(turtle_movement[1])
+    turtle.penup()
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[3])
+    next_number()
 
 
 def number_eight():
-    pass
+    turtle.pendown()
+    turtle.forward(turtle_movement[1])
+    turtle.backward(turtle_movement[1])
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[3])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.backward(turtle_movement[1])
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    next_number()
 
 
 def number_nine():
-    pass
+    turtle.pendown()
+    turtle.forward(turtle_movement[1])
+    turtle.backward(turtle_movement[1])
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.right(turtle_rotation[2])
+    turtle.forward(turtle_movement[2])
+    turtle.penup()
+    turtle.left(turtle_rotation[2])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.pendown()
+    turtle.forward(turtle_movement[1])
+    next_number()
 
 
 def number_zero():
-    pass
+    turtle.pendown()
+    turtle.forward(turtle_movement[1])
+    turtle.backward(turtle_movement[1])
+    turtle.right(turtle_rotation[1])
+    turtle.forward(turtle_movement[3])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[1])
+    turtle.left(turtle_rotation[1])
+    turtle.forward(turtle_movement[3])
+    next_number()
 
 
 def post_index_maker():
-    number_one()
-    number_three()
-    number_four()
+    number_zero()
+    number_six()
+    number_nine()
+    number_two()
 
+
+start_pozition()
 post_index_maker()
 turtle.penup()
 turtle.forward(1000)
